190a1467f7c830afff6c0613441cac6a262da3fd
Christensen, Steinar <steinar.christensen@nordicsemi.no>
2018-10-01 16:12:41 +0200

    Added sigmoind memory python script

---
+++ Sigmoid/ip/Sigmoid/rtl/SigmoidMemoryData/writeSigmoidMemory.py
@@ -0,0 +1,39 @@
+#This script writes the sigmoid function memory. The sigmoid function is 1.0/(1.0 + exp(-x)) 
+#The input is a value between -inf and inf. The output is between 0 and 1
+#dataWidth is dynamic. Since the output always is less than 1, each memory entry is of the form [dataWidth,dataWidth]
+#An input value greater than 4 means output = 1 and saturation is reached. Less than -4 means output = 0
+
+import numpy as np
+
+if __name__== "__main__":
+    dataWidth = 4
+    fileName = "sigmoidMemory.txt"
+    rang = np.arange(-4,4,8*2**(-dataWidth)) #Create range with stepsize
+    sig = np.zeros(2**dataWidth)
+    k = 0
+    for i in np.nditer(rang):
+        sig[k] = (1.0/(1.0 + np.exp(-i)))
+        k += 1
+
+    print(sig)
+    print(len(sig))
+
+    sig_shifted = sig * 2**dataWidth
+     
+    sig_shifted.astype(np.int64)
+    print(sig_shifted)
+    print(len(sig_shifted))
+      
+    sig_quantized = sig_shifted
+    for i in range (0, 2**dataWidth):
+        sig_quantized[i] = np.floor(sig_shifted[i])
+        print(type(sig_shifted[i]))
+
+    print(sig_quantized)
+    print(len(sig_quantized))
+
+    f = open(fileName,'w')
+
+
+    #Use bin() function to convert to binary
+
